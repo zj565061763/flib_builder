9a249bc94e7e60df435654ad7f89394abaa184f0
zhengjun <565061763@qq.com>
2019-04-27 11:12:45 +0800

    export

--- lib/flib_builder.dart
+++ lib/flib_builder.dart
@@ -1,6 +1,9 @@
 library flib_builder;
 
 export 'src/widget/button_builder.dart';
+export 'src/widget/column_builder.dart';
 export 'src/widget/container_builder.dart';
 export 'src/widget/gesture_detector_builder.dart';
+export 'src/widget/row_builder.dart';
+export 'src/widget/sized_box_builder.dart';
 export 'src/widget/text_builder.dart';
