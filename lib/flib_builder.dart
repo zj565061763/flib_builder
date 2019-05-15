b20b63989abc3535e8b338f4c4e92e8e2ded9e88
zjun <565061763@qq.com>
2019-05-15 15:53:44 +0800

    remove gesture builder

--- lib/flib_builder.dart
+++ lib/flib_builder.dart
@@ -5,7 +5,6 @@ export 'src/widget/button_builder.dart';
 export 'src/widget/card_builder.dart';
 export 'src/widget/column_builder.dart';
 export 'src/widget/container_builder.dart';
-export 'src/widget/gesture_detector_builder.dart';
 export 'src/widget/row_builder.dart';
 export 'src/widget/sized_box_builder.dart';
 export 'src/widget/stack_builder.dart';
