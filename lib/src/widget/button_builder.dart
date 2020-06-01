01af62ac6c9eb9a1892019b499b9ed957c0d66ae
zjun <565061763@qq.com>
2020-06-01 17:25:58 +0800

    FFlatButtonBuilder

--- lib/src/widget/button_builder.dart
+++ lib/src/widget/button_builder.dart
@@ -121,47 +121,6 @@ class FFlatButtonBuilder extends FWidgetBuilder
       child: this.child,
     );
   }
-
-  FlatButton buildIcon({
-    Key key,
-    @required VoidCallback onPressed,
-    ValueChanged<bool> onHighlightChanged,
-    ButtonTextTheme textTheme,
-    Color textColor,
-    Color disabledTextColor,
-    Color color,
-    Color disabledColor,
-    Color highlightColor,
-    Color splashColor,
-    Brightness colorBrightness,
-    EdgeInsetsGeometry padding,
-    ShapeBorder shape,
-    Clip clipBehavior,
-    MaterialTapTargetSize materialTapTargetSize,
-    @required Widget icon,
-    @required Widget label,
-  }) {
-    return FlatButton.icon(
-      key: key ?? this.key,
-      onPressed: onPressed ?? this.onPressed,
-      onHighlightChanged: onHighlightChanged ?? this.onHighlightChanged,
-      textTheme: textTheme ?? this.textTheme,
-      textColor: textColor ?? this.textColor,
-      disabledTextColor: disabledTextColor ?? this.disabledTextColor,
-      color: color ?? this.color,
-      disabledColor: disabledColor ?? this.disabledColor,
-      highlightColor: highlightColor ?? this.highlightColor,
-      splashColor: splashColor ?? this.splashColor,
-      colorBrightness: colorBrightness ?? this.colorBrightness,
-      padding: padding ?? this.padding,
-      shape: shape ?? this.shape,
-      clipBehavior: clipBehavior ?? this.clipBehavior,
-      materialTapTargetSize:
-          materialTapTargetSize ?? this.materialTapTargetSize,
-      icon: icon,
-      label: label,
-    );
-  }
 }
 
 class FRaisedButtonBuilder extends FWidgetBuilder
