9ed200df0f0f7892e7fbb05010c040a2dd71b032
zjun <565061763@qq.com>
2020-06-01 17:31:50 +0800

    RaisedButton

--- lib/src/widget/button_builder.dart
+++ lib/src/widget/button_builder.dart
@@ -126,142 +126,63 @@ class FFlatButtonBuilder extends FWidgetBuilder
 class FRaisedButtonBuilder extends FWidgetBuilder
     with FMixinKeyHolder, FMixinChildHolder {
   VoidCallback onPressed;
+  VoidCallback onLongPress;
   ValueChanged<bool> onHighlightChanged;
   ButtonTextTheme textTheme;
   Color textColor;
   Color disabledTextColor;
   Color color;
   Color disabledColor;
+  Color focusColor;
+  Color hoverColor;
   Color highlightColor;
   Color splashColor;
   Brightness colorBrightness;
   double elevation;
+  double focusElevation;
+  double hoverElevation;
   double highlightElevation;
   double disabledElevation;
   EdgeInsetsGeometry padding;
+  VisualDensity visualDensity;
   ShapeBorder shape;
-  Clip clipBehavior;
+  Clip clipBehavior = Clip.none;
+  FocusNode focusNode;
+  bool autofocus = false;
   MaterialTapTargetSize materialTapTargetSize;
   Duration animationDuration;
 
-  FRaisedButtonBuilder({
-    Key key,
-    Widget child,
-    this.onPressed,
-    this.onHighlightChanged,
-    this.textTheme,
-    this.textColor,
-    this.disabledTextColor,
-    this.color,
-    this.disabledColor,
-    this.highlightColor,
-    this.splashColor,
-    this.colorBrightness,
-    this.elevation,
-    this.highlightElevation,
-    this.disabledElevation,
-    this.padding,
-    this.shape,
-    this.clipBehavior = Clip.none,
-    this.materialTapTargetSize,
-    this.animationDuration,
-  }) {
-    this.key = key;
-    this.child = child;
-  }
-
   @override
-  Widget build({
-    Key key,
-    @required VoidCallback onPressed,
-    ValueChanged<bool> onHighlightChanged,
-    ButtonTextTheme textTheme,
-    Color textColor,
-    Color disabledTextColor,
-    Color color,
-    Color disabledColor,
-    Color highlightColor,
-    Color splashColor,
-    Brightness colorBrightness,
-    double elevation,
-    double highlightElevation,
-    double disabledElevation,
-    EdgeInsetsGeometry padding,
-    ShapeBorder shape,
-    Clip clipBehavior = Clip.none,
-    MaterialTapTargetSize materialTapTargetSize,
-    Duration animationDuration,
-    Widget child,
-  }) {
+  Widget build() {
     return RaisedButton(
-      key: key ?? this.key,
-      onPressed: onPressed ?? this.onPressed,
-      onHighlightChanged: onHighlightChanged ?? this.onHighlightChanged,
-      textTheme: textTheme ?? this.textTheme,
-      textColor: textColor ?? this.textColor,
-      disabledTextColor: disabledTextColor ?? this.disabledTextColor,
-      color: color ?? this.color,
-      disabledColor: disabledColor ?? this.disabledColor,
-      highlightColor: highlightColor ?? this.highlightColor,
-      splashColor: splashColor ?? this.splashColor,
-      colorBrightness: colorBrightness ?? this.colorBrightness,
-      elevation: elevation ?? this.elevation,
-      highlightElevation: highlightElevation ?? this.highlightElevation,
-      disabledElevation: disabledElevation ?? this.disabledElevation,
-      padding: padding ?? this.padding,
-      shape: shape ?? this.shape,
-      clipBehavior: clipBehavior ?? this.clipBehavior,
-      materialTapTargetSize:
-          materialTapTargetSize ?? this.materialTapTargetSize,
-      animationDuration: animationDuration ?? this.animationDuration,
-      child: child ?? this.child,
-    );
-  }
-
-  RaisedButton buildIcon({
-    Key key,
-    @required VoidCallback onPressed,
-    ValueChanged<bool> onHighlightChanged,
-    ButtonTextTheme textTheme,
-    Color textColor,
-    Color disabledTextColor,
-    Color color,
-    Color disabledColor,
-    Color highlightColor,
-    Color splashColor,
-    Brightness colorBrightness,
-    double elevation,
-    double highlightElevation,
-    double disabledElevation,
-    ShapeBorder shape,
-    Clip clipBehavior,
-    MaterialTapTargetSize materialTapTargetSize,
-    Duration animationDuration,
-    @required Widget icon,
-    @required Widget label,
-  }) {
-    return RaisedButton.icon(
-      key: key ?? this.key,
-      onPressed: onPressed ?? this.onPressed,
-      onHighlightChanged: onHighlightChanged ?? this.onHighlightChanged,
-      textTheme: textTheme ?? this.textTheme,
-      textColor: textColor ?? this.textColor,
-      disabledTextColor: disabledTextColor ?? this.disabledTextColor,
-      color: color ?? this.color,
-      disabledColor: disabledColor ?? this.disabledColor,
-      highlightColor: highlightColor ?? this.highlightColor,
-      splashColor: splashColor ?? this.splashColor,
-      colorBrightness: colorBrightness ?? this.colorBrightness,
-      elevation: elevation ?? this.elevation,
-      highlightElevation: highlightElevation ?? this.highlightElevation,
-      disabledElevation: disabledElevation ?? this.disabledElevation,
-      shape: shape ?? this.shape,
-      clipBehavior: clipBehavior ?? this.clipBehavior,
-      materialTapTargetSize:
-          materialTapTargetSize ?? this.materialTapTargetSize,
-      animationDuration: animationDuration ?? this.animationDuration,
-      icon: icon,
-      label: label,
+      key: this.key,
+      onPressed: this.onPressed,
+      onLongPress: this.onLongPress,
+      onHighlightChanged: this.onHighlightChanged,
+      textTheme: this.textTheme,
+      textColor: this.textColor,
+      disabledTextColor: this.disabledTextColor,
+      color: this.color,
+      disabledColor: this.disabledColor,
+      focusColor: this.focusColor,
+      hoverColor: this.hoverColor,
+      highlightColor: this.highlightColor,
+      splashColor: this.splashColor,
+      colorBrightness: this.colorBrightness,
+      elevation: this.elevation,
+      focusElevation: this.focusElevation,
+      hoverElevation: this.hoverElevation,
+      highlightElevation: this.highlightElevation,
+      disabledElevation: this.disabledElevation,
+      padding: this.padding,
+      visualDensity: this.visualDensity,
+      shape: this.shape,
+      clipBehavior: this.clipBehavior,
+      focusNode: this.focusNode,
+      autofocus: this.autofocus,
+      materialTapTargetSize: this.materialTapTargetSize,
+      animationDuration: this.animationDuration,
+      child: this.child,
     );
   }
 }
