c9fbe6d3d9be581a6014779c05c398c4cc760457
zjun <565061763@qq.com>
2020-06-04 11:42:31 +0800

    gesture_detector_builder

--- lib/flib_builder.dart
+++ lib/flib_builder.dart
@@ -4,6 +4,7 @@ export 'src/widget/button_builder.dart';
 export 'src/widget/card_builder.dart';
 export 'src/widget/column_builder.dart';
 export 'src/widget/container_builder.dart';
+export 'src/widget/gesture_detector_builder.dart';
 export 'src/widget/image_builder.dart';
 export 'src/widget/row_builder.dart';
 export 'src/widget/sized_box_builder.dart';
