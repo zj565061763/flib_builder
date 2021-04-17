41f8cd7edf1b51b3026ad7be760c4857bf29a489
zjun <565061763@qq.com>
2021-04-17 14:46:26 +0800

    text_builder

--- lib/src/widget/text_builder.dart
+++ lib/src/widget/text_builder.dart
@@ -2,41 +2,25 @@ import 'package:flib_builder/src/widget/widget_builder.dart';
 import 'package:flutter/material.dart';
 
 class FBText extends FWidgetBuilder {
-  String data;
-  TextStyle style;
-  StrutStyle strutStyle;
-  TextAlign textAlign;
-  TextDirection textDirection;
-  Locale locale;
-  bool softWrap;
-  TextOverflow overflow;
-  double textScaleFactor;
-  int maxLines;
-  String semanticsLabel;
-  TextWidthBasis textWidthBasis;
-  TextHeightBehavior textHeightBehavior;
-
-  FBText(
-    this.data, {
-    this.style,
-    this.strutStyle,
-    this.textAlign,
-    this.textDirection,
-    this.locale,
-    this.softWrap,
-    this.overflow,
-    this.textScaleFactor,
-    this.maxLines,
-    this.semanticsLabel,
-    this.textWidthBasis,
-    this.textHeightBehavior,
-  });
+  String data = "";
+  TextStyle? style;
+  StrutStyle? strutStyle;
+  TextAlign? textAlign;
+  TextDirection? textDirection;
+  Locale? locale;
+  bool? softWrap;
+  TextOverflow? overflow;
+  double? textScaleFactor;
+  int? maxLines;
+  String? semanticsLabel;
+  TextWidthBasis? textWidthBasis;
+  TextHeightBehavior? textHeightBehavior;
 
   @protected
   @override
   Widget buildImpl() {
     return Text(
-      this.data ?? '',
+      this.data,
       key: this.key,
       style: this.style,
       strutStyle: this.strutStyle,
@@ -55,38 +39,24 @@ class FBText extends FWidgetBuilder {
 }
 
 class FBRichText extends FWidgetBuilder {
-  TextSpan text;
-  TextAlign textAlign;
-  TextDirection textDirection;
+  InlineSpan text = TextSpan(text: "");
+  TextAlign textAlign = TextAlign.start;
+  TextDirection? textDirection;
   bool softWrap = true;
-  TextOverflow overflow;
-  double textScaleFactor;
-  int maxLines;
-  Locale locale;
-  StrutStyle strutStyle;
-  TextWidthBasis textWidthBasis;
-  TextHeightBehavior textHeightBehavior;
-
-  FBRichText({
-    @required this.text,
-    this.textAlign = TextAlign.start,
-    this.textDirection,
-    this.softWrap = true,
-    this.overflow = TextOverflow.clip,
-    this.textScaleFactor = 1.0,
-    this.maxLines,
-    this.locale,
-    this.strutStyle,
-    this.textWidthBasis = TextWidthBasis.parent,
-    this.textHeightBehavior,
-  });
+  TextOverflow overflow = TextOverflow.clip;
+  double textScaleFactor = 1.0;
+  int? maxLines;
+  Locale? locale;
+  StrutStyle? strutStyle;
+  TextWidthBasis textWidthBasis = TextWidthBasis.parent;
+  TextHeightBehavior? textHeightBehavior;
 
   @protected
   @override
   Widget buildImpl() {
     return RichText(
       key: this.key,
-      text: this.text ?? TextSpan(text: ''),
+      text: this.text,
       textAlign: this.textAlign,
       textDirection: this.textDirection,
       softWrap: this.softWrap,
